7301d001c4f0adef9acdeefb0e5249d7691e0f30
leticiamantovani <leticiamantovani159@gmail.com>
2025-02-22 17:10:30 -0300

    feature/T03 - adding new user login api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+import express from 'express';
+import { LoginRouter } from './routes/login';
+import { LoginController } from './controllers/loginController';
+import { LoginService } from './services/loginService';
+import config from './config';
+
+const app = express();
+const PORT = config.port;
+
+app.use(express.json()); 
+
+const loginService = new LoginService();
+const loginController = new LoginController(loginService);
+const loginRouter = new LoginRouter(loginController);
+app.use('/login', loginRouter.getRouter());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ src/services/loginService.js
@@ -0,0 +1,8 @@
+
+
+export class LoginService {
+    //add validation for username and password
+    async loginValidation(username, password) { 
+        console.log(username, password);
+    }
+};
