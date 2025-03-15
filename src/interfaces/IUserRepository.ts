f9fa9fa1350f036ac6d5482457aa38ef6a183575
leticiamantovani <leticiamantovani159@gmail.com>
2025-03-14 22:36:38 -0300

    fix: fix functions types

--- src/interfaces/IUserRepository.ts
+++ src/interfaces/IUserRepository.ts
@@ -1,9 +1,9 @@
-import { Document } from "mongoose";
+import { Document, ObjectId } from "mongoose";
 import IUser from "./IUser";
 
 export interface IUserRepository {
     createUser(username: string, password: string, role: string): Promise<Document>;
     getUser(username: string): Promise<IUser | null>;
-    updateUser(username: string, password: string, role: string): Promise<any>;
-    deleteUser(username: string): Promise<any>;
+    updateUser(userId: ObjectId, newUsername: string): Promise<any>;
+    deleteUser(userId: string): Promise<any>;
 }
